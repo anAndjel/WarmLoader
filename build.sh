4d9521060c7ab32b32635e881e23c8d16d98027e
Alex Tavares <alexdasilvamassanatavares@gmail.com>
2025-12-24 19:34:22 +0100

    sdfsdfojvdv

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd /home/alex/coding/WarmLoader || exit 1
-
-set -e
-
-mkdir -p build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -B build -S .
-cmake --build build --target warmloader -- -j "$(nproc)"
